b1fdbf5850b04249ee93a724d8583c9401dff5df
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-05-08 04:46:42 +0000

    stage 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+  validates :name,  presence: true, length: { minimum: 4 }
+  RMIT_EMAIL_REGEX = /[a-z]+\.[a-z]+@rmit\.edu\.au/i
+  validates :email, presence: true, length: { minimum: 4 },
+                    format: { with: RMIT_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
                     
   has_secure_password  
-  validates :password, presence: true, length: { minimum: 6 }
+  
+  PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[a-zA-Z0-9]/
+  validates :password, presence: true, 
+                       length: { minimum: 8 },
+                       format:{with: PASSWORD_REGEX}
 end
