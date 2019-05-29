84c05841e0e4905e7044765f521666350fe142c0
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-05-29 12:09:24 +0000

    minor errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash.now[:success] = "Sign up successfully!"
-      redirect_to home
+      redirect_to course_home_url
     else
       errors = ["Please correct following errors:","Name cant be blank","Name is too short(minimum is 4)","Email cannot be blank","Email is too short(minimum is 4)","Email registration is only for RMIT staff","Pass word cant be blank","Password must contain at least one uppercase letter, one lowercase letter, one number, one special character and 8+characters"]
       flash.now[:danger] = errors.join("<br>").html_safe
