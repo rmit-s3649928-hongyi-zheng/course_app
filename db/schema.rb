a0217e83abd434e89490500229e04b571cf1b152
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-05-29 11:58:53 +0000

    final update add all the files which are forgotten to commmit,fix minor errors,deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,8 @@ ActiveRecord::Schema.define(version: 20190528072948) do
     t.integer "category_id", null: false
   end
 
-  create_table "coordinators", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 # Could not dump table "courses" because of following StandardError
-#   Unknown type '' for column 'description'
+#   Unknown type 'String' for column 'description'
 
   create_table "courses_locations", force: :cascade do |t|
     t.integer "course_id", null: false
