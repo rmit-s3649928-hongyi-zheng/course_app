95f60507b4b04ad44672104d0a5c023de6be3ec4
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-05-29 12:47:07 +0000

    modified migration files for heroku deployment

--- db/migrate/20190528044746_add_description_to_course.rb
+++ db/migrate/20190528044746_add_description_to_course.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToCourse < ActiveRecord::Migration[5.1]
   def change
-    add_column :courses, :description, :String,default:"this is a default description"
+    add_column :courses, :description, :string,default:"this is a default description"
   end
 end
--- db/migrate/20190528053904_add_image_to_course.rb
+++ db/migrate/20190528053904_add_image_to_course.rb
@@ -1,5 +1,5 @@
 class AddImageToCourse < ActiveRecord::Migration[5.1]
   def change
-    add_column :courses, :image, :String
+    add_column :courses, :image, :string
   end
 end
