80538b2fcd98a98248a57c5c08ad47ba5682f6a6
Hongyi Zheng <s3649928@student.rmit.edu.au>
2019-10-10 01:17:23 +0000

    enables google map api functions

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -70,7 +70,7 @@ class LocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-      params.require(:location).permit(:building, :floor, :room)
+      params.require(:location).permit(:building, :floor, :room,:lat,:lng)
     end
     
     # GET /locations/1/roll
